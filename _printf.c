8c919cd86f498d34944ff58cc2cb0a8638eb87d7
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-05-22 01:19:43 +0100

    setting up continues

---
+++ _printf.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _printf - my own printf function
+ *
+ * Return: void
+ */
+int _printf(const char *format, ...)
+{
+	int i;
+	va_list args;
+
+	i = 0;
+	va_start(args, format);
+	while (format && format[i])
+	{
+	}
+}
